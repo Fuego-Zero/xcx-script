1fdeb65905c74d2adb6e02dce86e2fbd59f263dd
Fuego <fuegozero35@gmail.com>
2019-04-10 14:22:15 +0800

    暂时移除关闭微信开发者工具的功能

--- script/clean.js
+++ script/clean.js
@@ -1,6 +1,4 @@
 // eslint-disable-next-line
 console.log('\033[2J')
-const exec = require('child_process').exec
-exec(`taskkill /f /im "wechatdevtools.exe"`, function () {
-  require('./operation/clean')()
-})
+require('./operation/clean')()  
+
--- script/operation/clean.js
+++ script/operation/clean.js
@@ -6,13 +6,18 @@ const log = require('../log')
 function clean () {
   return new Promise((resolve, reject) => {
     log.info('开始清理目录。')
-    del.sync([`${conf.outputDir}/**`, `!${conf.outputDir}`])
-    del.sync([`${conf.outputDir}`])
 
-    setTimeout(() => {
-      log.info('目录清理完毕。')
-      resolve()
-    }, 1000)
+    ;(function exec () {
+      try {
+        del.sync([`${conf.outputDir}/**`, `!${conf.outputDir}`])
+        del.sync([`${conf.outputDir}`])
+      } catch (error) {
+        exec()
+      }
+    })()
+
+    log.info('目录清理完毕。')
+    resolve()
   })
 }
 
